f02f33cfcd6bb8a779f63805c533c7c47c9ff507
Manmellon <24492231+Manmellon@users.noreply.github.com>
2019-03-21 21:08:11 +0300

    Created start point

--- syntax.cpp
+++ syntax.cpp
@@ -4,6 +4,9 @@ using namespace std;
 enum lexeme{NAME,CONSTANT,PLUS,MINUS,MULT,DIV,ASSIGNMENT,OPPNUMBER,EQUAL,
 			AND,OR,DUP,DROP,SWAP,PUT,ROT,IF,WHILE,FUNC_OPEN,FUNC_CLOSE,ERROR};
 const int STMT = ERROR+1;
+
+int curLexeme;
+
 struct node
 {
 	struct node *child1,*child2,*child3;
@@ -16,9 +19,14 @@ node* createNode(int _type)
 	n->child1=n->child2=n->child3=NULL;
 	return n;
 }
+node* program()
+{
+	node *n = createNode(STMT);
+	return n;
+}
 int main(int argc, char **argv)
 {
-	
+	curLexeme = 0;
 	return 0;
 }
 
