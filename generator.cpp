6f1cb1d48dd1d8d921fe8ea07e5cb708e67d8e6a
Manmellon <24492231+Manmellon@users.noreply.github.com>
2019-05-25 15:07:04 +0300

    Added empty cases

--- generator.cpp
+++ generator.cpp
@@ -45,6 +45,67 @@ string treeToCode(node* tree)
 			if(tree->child1)code+=treeToCode(tree->child1);//looks bad
 			if(tree->child2)code+=treeToCode(tree->child2);
 		}break;
+		case NAME:
+		{
+		}break;
+		case CONSTANT:
+		{
+		}break;
+		case PLUS:
+		{
+		}break;
+		case MINUS:
+		{
+		}break;
+		case MULT:
+		{
+		}break;
+		case DIV:
+		{
+		}break;
+		case ASSIGNMENT:
+		{
+		}break;
+		case OPPNUMBER:
+		{
+		}break;
+		case EQUAL:
+		{
+		}break;
+		case AND:
+		{
+		}break;
+		case OR:
+		{
+		}break;
+		case DUP:
+		{
+		}break;
+		case DROP:
+		{
+		}break;
+		case SWAP:
+		{
+		}break;
+		case PUT:
+		{
+		}break;
+		case ROT:
+		{
+		}break;
+		case IF:
+		{
+		}break;
+		case WHILE:
+		{
+		}break;
+		case FUNC_OPEN:
+		{
+		}break;
+		case FUNC_CLOSE:
+		{
+		}break;
+		default:break;
 	}
 	return code;
 }
